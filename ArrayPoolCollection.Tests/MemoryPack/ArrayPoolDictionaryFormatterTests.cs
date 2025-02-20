3d5187d8fa4d3694240f5fe77e2ab5ce27659144
Andante <andante.soft@gmail.com>
2025-02-21 08:34:00 +0900

    remove unnecessary usings

--- ArrayPoolCollection.Tests/MemoryPack/ArrayPoolDictionaryFormatterTests.cs
+++ ArrayPoolCollection.Tests/MemoryPack/ArrayPoolDictionaryFormatterTests.cs
@@ -1,4 +1,3 @@
-using System.Runtime;
 using MemoryPack;
 
 namespace ArrayPoolCollection.MemoryPack.Tests;
--- ArrayPoolCollection.Tests/MemoryPack/ArrayPoolQueueFormatterTests.cs
+++ ArrayPoolCollection.Tests/MemoryPack/ArrayPoolQueueFormatterTests.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using MemoryPack;
 
 namespace ArrayPoolCollection.MemoryPack.Tests;
